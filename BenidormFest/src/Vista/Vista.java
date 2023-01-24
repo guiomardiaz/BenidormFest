3147dd4658de7cf51d1e088af91093c0f0e819d2
guiomardiaz <guiomir2507@gmail.com>
2023-01-24 19:50:40 +0100

    Vista Empezar

--- BenidormFest/src/Vista/Vista.java
+++ BenidormFest/src/Vista/Vista.java
@@ -7,16 +7,23 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import Controlador.Controlador;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JLabel;
 
 public class Vista extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtBienvenidoALas;
+	public JPanel PanelInicio;
 	public JButton btnEmpezar;
+	public JLabel lblFoto;
+	public JLabel lblBienvenido;
+	public JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -42,22 +49,37 @@ public class Vista extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1152, 673);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		 btnEmpezar = new JButton("EMPEZAR");
-		btnEmpezar.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnEmpezar.setBounds(419, 516, 214, 98);
-		contentPane.add(btnEmpezar);
+		PanelInicio = new JPanel();
+		PanelInicio.setBackground(new Color(0, 0, 0));
+		PanelInicio.setBounds(0, 0, 1138, 636);
+		contentPane.add(PanelInicio);
+		PanelInicio.setLayout(null);
 		
-		txtBienvenidoALas = new JTextField();
-		txtBienvenidoALas.setHorizontalAlignment(SwingConstants.CENTER);
-		txtBienvenidoALas.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		txtBienvenidoALas.setText("Bienvenido a las votaciones de Benidorm Fest");
-		txtBienvenidoALas.setBounds(293, 39, 437, 53);
-		contentPane.add(txtBienvenidoALas);
-		txtBienvenidoALas.setColumns(10);
+		btnEmpezar = new JButton("EMPEZAR");
+		btnEmpezar.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnEmpezar.setBounds(430, 505, 214, 98);
+		PanelInicio.add(btnEmpezar);
+		
+		lblFoto = new JLabel("");
+		lblFoto.setBounds(399, 163, 264, 120);
+		PanelInicio.add(lblFoto);
+		
+		lblBienvenido = new JLabel("Bienvenido a las votaciones de Benidorm Fest");
+		lblBienvenido.setForeground(Color.WHITE);
+		lblBienvenido.setFont(new Font("Tahoma", Font.PLAIN, 32));
+		lblBienvenido.setBounds(217, 47, 666, 74);
+		PanelInicio.add(lblBienvenido);
+		
+		lblNewLabel = new JLabel("Aquí podrás ver en tiempo real los votos realizados");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel.setBounds(285, 341, 531, 37);
+		PanelInicio.add(lblNewLabel);
 	}
 }
