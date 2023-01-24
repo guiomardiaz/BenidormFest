4028dcbd086897135ee0b91a24b83e413ae04e28
caram <carmelolosa@gmail.com>
2023-01-24 18:12:32 +0100

    conexion

--- BenidormFest/src/Controlador/Controlador.java
+++ BenidormFest/src/Controlador/Controlador.java
@@ -2,9 +2,12 @@ package Controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -35,9 +38,22 @@ import Vista.Vista;
 		}//end action listener
 		
 		
-		private Connection crearConexion() {
-			// TODO Auto-generated method stub
-			return null;
+		private Connection crearConexion() throws ClassNotFoundException, SQLException {
+			Connection conexion = null;
+			Properties propiedades = new Properties();
+			try {
+				propiedades.load(new FileReader("src/Modelo/database.properties"));
+				String driver = propiedades.getProperty("database.driver");
+				String url = propiedades.getProperty("database.url");
+				String usuario = propiedades.getProperty("database.user");
+				String contrasenia = propiedades.getProperty("database.password");
+				Class.forName(driver);
+				conexion = DriverManager.getConnection(url, usuario, contrasenia);
+				conexion.setAutoCommit(false);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return conexion;
 		}
 		
 	}//end controlador
