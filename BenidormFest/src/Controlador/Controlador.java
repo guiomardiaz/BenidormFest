c4024b11ca6664ef42cf1697ad8443cdbe7d7cf5
caram <carmelolosa@gmail.com>
2023-01-26 10:24:50 +0100

    Metodo para ajustar los habitantes realizado

--- BenidormFest/src/Controlador/Controlador.java
+++ BenidormFest/src/Controlador/Controlador.java
@@ -6,7 +6,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 import javax.sound.sampled.LineUnavailableException;
@@ -27,7 +30,12 @@ import Vista.Vista;
 			vista.btnEmpezar.addActionListener(this);
 			conexion = crearConexion();
 			
-			double[] numEdades = recuperar1();
+			System.out.println("Conexion realizada");
+			
+			int[] habitantesAndalucia= recuperarHabitantes("Andalucia", conexion); 
+			for(int i = 0; i<habitantesAndalucia.length; i++) {
+			System.out.println(habitantesAndalucia[i]);
+			}
 			
 		}
 
@@ -61,13 +69,54 @@ import Vista.Vista;
 		}//end crear conexion
 
 		
-
-		private double[] recuperar1() {
-			double[] edades = new double[6];
-			String sentencia = "SELECT RANGO_1_9, RANGO_10_17, RANGO_18_25, RANGO_26_40, RANGO_41_65, RANGO_MAS_66";
-			
-			return edades;
+		private static int[] recuperarHabitantes(String comunidad, Connection conexion) {
+			int[] aux = new int[7];
+			double[] porcentajes = new double[6];
+			int[] habitantes = new int[6];
+			String sentencia = "SELECT RANGO_1_9, RANGO_10_17, RANGO_18_25, RANGO_26_40, RANGO_41_65, RANGO_MAS_66, TOTAL_HABITANTES\r\n"
+					+ "FROM PORCENTAJES_RANGOEDAD\r\n"
+					+ "WHERE NOMBRE_COMUNIDAD LIKE ?";
+			PreparedStatement statement = null;
+			Statement statement2 = null;
+			ResultSet resultSet = null;
+			ResultSet resultSet2 = null;
+			try {
+				statement = conexion.prepareStatement(sentencia);
+				statement.setString(1, comunidad);
+				resultSet = statement.executeQuery();
+				if(resultSet.next()) {
+					aux[0]= resultSet.getInt("RANGO_1_9");
+					aux[1]= resultSet.getInt("RANGO_10_17");
+					aux[2]= resultSet.getInt("RANGO_18_25");
+					aux[3]= resultSet.getInt("RANGO_26_40");
+					aux[4]= resultSet.getInt("RANGO_41_65");
+					aux[5]= resultSet.getInt("RANGO_MAS_66");
+					aux[6]= resultSet.getInt("TOTAL_HABITANTES");
+				}
+				//PARTE 2 -------------------------------------------------------
+				//---------------------------------------------------------------
+				
+				sentencia = "SELECT PORCENTAJE\r\n"
+						+ "FROM PORCENTAJE_VOTACION_RANGO";
+				statement2 = conexion.createStatement();
+				resultSet2 = statement2.executeQuery(sentencia);
+				int contador=0;
+				while (resultSet2.next()) {
+					porcentajes[contador] = resultSet2.getDouble("PORCENTAJE");
+					contador++;
+				}
+				//PARTE 3-------------------------------------------------------
+				//--------------------------------------------------------------
+				for(int i = 0; i<porcentajes.length; i++) {
+					habitantes[i] =  (aux[i]*aux[6])/100;
+					habitantes[i] = (int) (habitantes[i]*porcentajes[i]);
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			return habitantes;
 		}
+
 		
 	}//end controlador
 
